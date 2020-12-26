4d93d8296e067f6bf516e693ced46ae81c9f11e7
khushimadan <63074598+khushimadan@users.noreply.github.com>
2020-12-27 01:34:06 +0530

    Create Evenodd.py

---
+++ Evenodd.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+remainder = number % 2;
+if remainder == 0:
+  print ("{0} is even".format(number))
+else: 
+  print ("{0} is odd and remainder is {1}.".format(number, remainder))
+  
+  
+© 2020 GitHub, Inc.
